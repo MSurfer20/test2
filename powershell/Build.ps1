b577a6d94c3228780fd1013e829920eabf25b805
MSurfer20 <suyash.mathur@research.iiit.ac.in>
2021-08-11 11:47:11 +0000

    Update docs from https://github.com/MSurfer20/zulip/commit/07333537e6ad17f2cdd0eb7c81b41f6c2b8a2add

--- powershell/Build.ps1
+++ powershell/Build.ps1
@@ -57,7 +57,7 @@ $Manifest = @{
     ModuleVersion = '0.1.2'
 
     RootModule = 'PSOpenAPITools.psm1'
-    Guid = 'FEC991B9-1F09-45BB-B47E-8B2B3F9F7FE4' # Has to be static, otherwise each new build will be considered different module
+    Guid = '{FEC991B9-1F09-45BB-B47E-8B2B3F9F7FE4}' # Has to be static, otherwise each new build will be considered different module
 
     PowerShellVersion = '6.2'
 
